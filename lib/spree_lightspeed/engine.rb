0559976f4893ea30a4cc9d36f56fa05b56c1603d
Denis Ivanov <visible@jumph4x.net>
2013-10-28 11:06:45 -0700

    Switching to RSpec

--- lib/spree_lightspeed/engine.rb
+++ lib/spree_lightspeed/engine.rb
@@ -1,5 +1,14 @@
 module SpreeLightspeed
   class Engine < ::Rails::Engine
     isolate_namespace SpreeLightspeed
+
+    config.generators do |g|
+      g.test_framework      :rspec,        :fixture => false
+      g.assets false
+      g.helper false
+    end
+
+    config.to_prepare do
+    end
   end
 end
--- test/spree_lightspeed_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SpreeLightspeedTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, SpreeLightspeed
-  end
-end
