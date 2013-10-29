010b8bec313abc31e63b35a518f6e814f3362d29
Denis Ivanov <visible@jumph4x.net>
2013-10-28 21:01:23 -0700

    Loadtime db migration path expansion

--- lib/spree_lightspeed/engine.rb
+++ lib/spree_lightspeed/engine.rb
@@ -12,5 +12,13 @@ module SpreeLightspeed
 
     config.to_prepare do
     end
+    
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
