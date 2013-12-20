975cbfe220f36ffe776414f31e3dd554a424cf8c
Sebastian Bean <sebastian@ginlanemedia.com>
2013-12-19 20:18:44 -0500

    removed hotlink migrations on principle

--- lib/spree_lightspeed/engine.rb
+++ lib/spree_lightspeed/engine.rb
@@ -19,13 +19,5 @@ module SpreeLightspeed
         require_dependency(c)
       end
     end
-
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
-        end
-      end
-    end
   end
 end
